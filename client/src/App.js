8317cb33e6eb8279b63d1b40ca565a9e925c455d
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2019-07-22 13:59:49 -0700

    Initial Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
+import { Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,9 +17,20 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div> 
+        {/* //Replace this Div with your Routes */}
+        <Route exact path="/" component={MovieList}/>
+
+      </div>
     </div>
   );
 };
+
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById("root")
+// );
 
 export default App;
